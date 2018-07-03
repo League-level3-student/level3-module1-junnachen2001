7bb7d37e29e3d35bb39fdd90e296c99ec6dd6c76
League <League@AdminisorsiMac2.attlocal.net>
2018-07-03 11:58:38 -0700

    Finishing HangMan

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,6 @@
 package _04_HangMan;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -9,21 +10,78 @@ import javax.swing.JPanel;
 
 public class HangMan{
 	JFrame frame;
-	JLabel label; 
+	JLabel label1, label2; 
 	JPanel panel;
-	public static void main(String[] args) {
-		int maxNum = Utilities.getTotalWordsInFile("dictionary.txt");
+	String word;
+	ArrayList<String> displayedWord, hiddenWord;
+	int lives = 10;
+	
+	public void createUI() {
+		frame  = new JFrame();
+		label1 = new JLabel();
+		label2 = new JLabel();
+		panel = new JPanel();
+		frame.add(panel);
+		panel.add(label1);
+		panel.add(label2);
+		label1.setText(convert(hiddenWord));
+		label2.setText("Lives: " + lives);
+		frame.setVisible(true);
+		frame.pack();
+	}
+	
+	public void setUp() {
+		int maxNum = Utilities.getTotalWordsInFile("src/dictionary.txt");
 		String number = JOptionPane.showInputDialog("Enter a number from 0 to " + maxNum);
 		int num = Integer.parseInt(number);
 		Stack<String> strings = new Stack<String>();
 		for(int i = 0; i < num; i++) {
-			String random = Utilities.readRandomLineFromFile("dictionary.txt");
+			String random = Utilities.readRandomLineFromFile("src/dictionary.txt");
 			if(strings.contains(random)) {
+				strings.pop();
 				i--;
 			}
 			else {
 				strings.push(random);
 			}
 		}
+		word = strings.pop();
+		displayedWord = new ArrayList<String>();
+		hiddenWord = new ArrayList<String>();
+		for(int i = 0; i < word.length(); i++) {
+			displayedWord.add(word.substring(i, i + 1));
+			hiddenWord.add("_ ");
+		}
+	}
+	
+	public String convert(ArrayList<String> array) {
+		String returnedWord = "";
+		for(int i = 0; i < array.size(); i++) {
+			returnedWord = returnedWord + array.get(i);
+		}
+		return returnedWord;
+	}
+	
+	public void playGame() {
+		while(!(convert(hiddenWord).equals(word))) {
+			String guess = JOptionPane.showInputDialog("Enter a guess");
+			for(int i = 0; i < word.length(); i++) {
+				if(displayedWord.get(i).equals(guess)) {
+					hiddenWord.set(i, displayedWord.get(i));
+					label1.setText(convert(hiddenWord));
+				}
+				else {
+					lives--;
+					label2.setText("Lives: " + lives);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		HangMan hang = new HangMan();
+		hang.setUp();
+		hang.createUI();
+		hang.playGame();
 	}
 }
